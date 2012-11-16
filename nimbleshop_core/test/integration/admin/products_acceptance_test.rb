736a648d143cb1e2f5cdde96f40f6dae93ce8a0c
Neeraj Singh <neerajdotname@gmail.com>
2012-11-16 12:45:20 -0500

    refactor existing test

--- nimbleshop_core/test/integration/admin/products_acceptance_test.rb
+++ nimbleshop_core/test/integration/admin/products_acceptance_test.rb
@@ -21,6 +21,21 @@ class ProductsAcceptanceTest < ActionDispatch::IntegrationTest
     assert page.has_content?('the very wicked name for product')
   end
 
+  test "edit a product" do
+    create(:product, name: 'test')
+    visit admin_path
+    click_link 'Products'
+    click_link 'Edit'
+
+    fill_in 'Name', with: 'the very wicked name for product'
+    fill_in 'Description', with: 'test desc for user'
+    fill_in 'Price', with: '45.99'
+    click_button 'Submit'
+
+    assert page.has_content?('Successfully updated')
+    assert page.has_content?('the very wicked name for product')
+  end
+
   test 'editing a product with picture' do
     create :picture
     visit admin_path
@@ -30,7 +45,7 @@ class ProductsAcceptanceTest < ActionDispatch::IntegrationTest
     assert_equal 1, page.all('ul.product_pictures li').count
   end
 
-  test "should not create product with wrong params" do
+  test "product creation with wrong params" do
     visit admin_path
     click_link 'Products'
     click_link 'add_new_product'
@@ -48,38 +63,8 @@ class ProductsAcceptanceTest < ActionDispatch::IntegrationTest
     refute page.has_content?("Price can't be blank")
   end
 
-  test "should delete a product" do
-    create(:product, name: 'ipad99')
-    visit admin_path
-    click_link 'Products'
-    click_link 'Delete'
-    assert page.has_content?("Successfully deleted")
-    refute page.has_content?('ipad99')
-  end
-
-end
-
-class ProductsAcceptance2Test < ActionDispatch::IntegrationTest
-
-  setup do
+  test "product update with wrong params" do
     create(:product, name: 'test')
-  end
-
-  test "should edit a product" do
-    visit admin_path
-    click_link 'Products'
-    click_link 'Edit'
-
-    fill_in 'Name', with: 'the very wicked name for product'
-    fill_in 'Description', with: 'test desc for user'
-    fill_in 'Price', with: '45.99'
-    click_button 'Submit'
-
-    assert page.has_content?('Successfully updated')
-    assert page.has_content?('the very wicked name for product')
-  end
-
-  test "should not allow to save product with wrong params" do
     visit admin_path
     click_link 'Products'
     click_link 'Edit'
@@ -94,6 +79,15 @@ class ProductsAcceptance2Test < ActionDispatch::IntegrationTest
     assert page.has_content?("Price is not a number")
   end
 
+  test "should delete a product" do
+    create(:product, name: 'ipad99')
+    visit admin_path
+    click_link 'Products'
+    click_link 'Delete'
+    assert page.has_content?("Successfully deleted")
+    refute page.has_content?('ipad99')
+  end
+
   test "show product" do
     create(:product, name: 'ipad', description: 'the description', price: 46.99)
     visit admin_path
@@ -102,4 +96,5 @@ class ProductsAcceptance2Test < ActionDispatch::IntegrationTest
 
     assert page.has_content?("Edit product")
   end
+
 end
