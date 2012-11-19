85e5c8af817df13262bd44a34032277b988e44e6
Neeraj Singh <neerajdotname@gmail.com>
2012-11-19 17:54:32 -0500

    wip

---
+++ themes/nimbleshop_tingly/app/assets/javascripts/nimbleshop_tingly/tingly.js
@@ -0,0 +1,24 @@
+
+//= require jquery-1.7.1
+//= require jquery-ui-1.8.16
+//= require jquery-ujs
+//= require accounting
+
+// bootstrap must be after jquery-ujs
+//= require twitter/bootstrap
+
+// fancybox is loaded using fancybox-rails gem
+//= require fancybox
+
+//= require utils/formatNumber
+//= require utils/formatPrice
+//= require loading_state_for_buttons
+
+//= require nimbleshop_tingly/shipping_address_same_as_billing_address
+//= require nimbleshop_tingly/thumbnail
+//= require nimbleshop_tingly/remove_item_from_cart
+//= require nimbleshop_tingly/load_states
+//= require nimbleshop_tingly/order
+//= require nimbleshop_tingly/enable_fancybox
+//= require nimbleshop_tingly/creditcard
+//= require nimbleshop_tingly/vendor/jquery.creditCardValidator
