553d702c2b5a40f5bcc9dd4e2c9d4107f801e80b
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2012-12-02 23:43:55 -0500

    code cleanup

--- nimbleshop_core/lib/nimbleshop/nimbleshop/payment_util.rb
+++ nimbleshop_core/lib/nimbleshop/nimbleshop/payment_util.rb
@@ -2,35 +2,29 @@ module Nimbleshop
   module PaymentUtil
     extend self
 
-    def billing_address_json(order)
-      a = order.final_billing_address
-
-      { address1: a.address1,
-        city:     a.city,
-        state:    a.state_name,
-        country:  a.country_name,
-        zip:      a.zipcode }
+    def billing_address_json(address)
+      { address1: address.address1,
+        city:     address.city,
+        state:    address.state_name,
+        country:  address.country_name,
+        zip:      address.zipcode }
     end
 
-    def shipping_address_json(order)
-      a = order.shipping_address
-
-      { first_name: a.first_name,
-        last_name:  a.last_name,
-        address1:   a.address1,
-        city:       a.city,
-        state:      a.state_name,
-        country:    a.country_name,
-        zip:        a.zipcode }
+    def shipping_address_json(address)
+      { first_name: address.first_name,
+        last_name:  address.last_name,
+        address1:   address.address1,
+        city:       address.city,
+        state:      address.state_name,
+        country:    address.country_name,
+        zip:        address.zipcode }
     end
 
     def activemerchant_options(order)
-      billing_address = { billing_address: billing_address_json(order) }
-      shipping_address = { shipping_address: shipping_address_json(order) }
-      misc = { order_id: order.number, email: order.email }
-      billing_address.merge(shipping_address).merge(misc)
+      { billing_address: billing_address_json(order.final_billing_address), 
+        shipping_address: shipping_address_json(order.shipping_address),
+        order_id: order.number, 
+        email: order.email }
     end
-
   end
 end
-
