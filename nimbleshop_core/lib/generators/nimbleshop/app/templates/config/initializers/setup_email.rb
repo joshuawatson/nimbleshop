0f83087b56c36d263f0619afa141845066948684
Neeraj Singh <neerajdotname@gmail.com>
2012-12-11 15:59:46 -0500

    fix mailtrap comments and address

--- nimbleshop_core/lib/generators/nimbleshop/app/templates/config/initializers/setup_email.rb
+++ nimbleshop_core/lib/generators/nimbleshop/app/templates/config/initializers/setup_email.rb
@@ -1,11 +1,14 @@
 if Rails.env.test?
 
+    # no configuration needed for test environment
+
 elsif Nimbleshop.config.deliver_email
 
   # Setup how you want email to be delivered
 
 else
 
+  # In environments like development and staging emails should go to  mailtrap.io
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name:      Nimbleshop.config.mailtrap.username,
