6017614c3e2b248f50c4b32967f8eace3e89a00d
Neeraj Singh <neerajdotname@gmail.com>
2012-12-11 23:40:01 -0500

    wip

--- nimbleshop_auth/lib/nimbleshop_auth/engine.rb
+++ nimbleshop_auth/lib/nimbleshop_auth/engine.rb
@@ -7,7 +7,7 @@ module NimbleshopAuth
       Rails.application.routes_reloader.reload!
     end
 
-    initializer 'nimbleshop_simply.precompile_assets' do |config|
+    initializer 'nimbleshop_auth.precompile_assets' do |config|
       Rails.application.config.assets.precompile += %w( nimbleshop_auth/auth.css )
       Rails.application.config.assets.precompile += %w( nimbleshop_auth/auth.js )
     end
