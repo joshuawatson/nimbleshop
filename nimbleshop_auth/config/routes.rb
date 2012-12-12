d91071e9ba3b7ab62d1525d7f9f1bb6dc8d6193e
Neeraj Singh <neerajdotname@gmail.com>
2012-12-11 23:38:21 -0500

    wip

--- nimbleshop_auth/config/routes.rb
+++ nimbleshop_auth/config/routes.rb
@@ -1,8 +1,8 @@
-Nimbleshop::Core::Engine.routes.draw do
+Rails.application.routes.draw do
   devise_for :users, { class_name: "User", module: :devise }
 end
 
-Nimbleshop::Core::Engine.routes.prepend do
+Rails.application.routes.prepend do
   as :user do
     get "users/edit" => "devise/registrations#edit", as: "edit_user_registration"
     put "users" => "devise/registrations#update", as: "user_registration"
